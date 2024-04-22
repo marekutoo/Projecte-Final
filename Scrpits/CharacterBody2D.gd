1158ba2c69022d94a562beb36480c1f221713e3e
Cianobacteria <165771255+Cianobacteria@users.noreply.github.com>
2024-04-22 12:28:10 +0200

    personatge amb fisiques

--- Scrpits/CharacterBody2D.gd
+++ Scrpits/CharacterBody2D.gd
@@ -3,31 +3,42 @@ extends CharacterBody2D
 var direccio := Vector2(0,0)
 var velocitat = 200
 var gravetat := Vector2.DOWN * 980
-
+var ajupit := false
 
 
 func _process(delta):
 	direccio = Vector2(0,0)
-	velocity.x = 200
+	velocity.x = 100
 	
 	if Input.is_action_just_pressed("saltar") and is_on_floor():
 		velocity += Vector2.UP * 330
+	if Input.is_action_just_pressed("ajupir-se"):# and is_on_floor():
+		ajupte()
 	
-	
-		
 	velocity += gravetat * delta
 	velocity +=  direccio.normalized() * velocitat
 	move_and_slide()
-	
-	if velocity.y < 0:
-		$Animacio.play('saltar')
-	elif velocity.y > 0:
-		$Animacio.play('saltar')
-	elif velocity.x > 0:
-		$Animacio.play('correr')
-		$Animacio.flip_h = false
-	elif velocity.x < 0:
-		$Animacio.play('correr')
-		$Animacio.flip_h = true
+	anima()
+
+func anima():
+	if not ajupit:
+		if velocity.y < 0:
+			$Animacio.play('saltar')
+		elif velocity.y > 0:
+			$Animacio.play('saltar')
+		elif velocity.x > 0:
+			$Animacio.play('correr')
 	else:
-		$Animacio.play('quiet')
+		$Animacio.play("ajup-te")
+
+func ajupte():
+	ajupit = true
+	$FormaAjupit.set_deferred("disabled", false)
+	$FormaDret.set_deferred("disabled", true)
+	
+func aixecat():
+	ajupit = false
+	$FormaAjupit.set_deferred("disabled", true)
+	$FormaDret.set_deferred("disabled", false)
+	
+	
