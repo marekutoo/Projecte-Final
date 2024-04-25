85666a3aca6c47b3566be5f0f2fa45fbf9fe33b0
Cianobacteria <165771255+Cianobacteria@users.noreply.github.com>
2024-04-25 09:59:28 +0200

    cop de puny del personatge

--- Scrpits/Personatge.gd
+++ Scrpits/Personatge.gd
@@ -4,17 +4,18 @@ var direccio := Vector2(0,0)
 var velocitat = 200
 var gravetat := Vector2.DOWN * 980
 var ajupit := false
+var atac := false
 
 
 func _process(delta):
 	direccio = Vector2(0,0)
 	velocity.x = 100
-	
+
 	if Input.is_action_just_pressed("saltar") and is_on_floor():
 		salta(400)
 	if Input.is_action_just_pressed("ajupir-se") and is_on_floor():
 		ajupte()
-	if Input.is_action_just_pressed("atac") and is_on_floor():
+	if Input.is_action_just_pressed("Atac") and is_on_floor():
 		cop_de_puny()
 
 	
@@ -24,16 +25,18 @@ func _process(delta):
 	anima()
 
 func anima():
-	if not ajupit:
-		if velocity.y < 0:
-			$Animacio.play('saltar')
-		elif velocity.y > 0:
-			$Animacio.play('saltar')
-		elif velocity.x > 0:
-			$Animacio.play('correr')
+	if not atac:
+		if not ajupit:
+			if velocity.y < 0:
+				$Animacio.play('saltar')
+			elif velocity.y > 0:
+				$Animacio.play('saltar')
+			elif velocity.x > 0:
+				$Animacio.play('correr')
+		else:
+			$Animacio.play("ajup-te")
 	else:
-		$Animacio.play("ajup-te")
-
+		$Animacio.play("atac")
 func ajupte():
 	ajupit = true
 	$FormaAjupit.set_deferred("disabled", false)
@@ -43,18 +46,20 @@ func aixecat():
 	ajupit = false
 	$FormaAjupit.set_deferred("disabled", true)
 	$FormaDret.set_deferred("disabled", false)
-	$Animacio.play("correr")
+
 	
 
 
 func _on_animacio_animation_finished():
 	if $Animacio.animation == "ajup-te":
 		aixecat()
+	elif $Animacio.animation == "atac":
+		atac = false
 
 func cop_de_puny():
+	atac = true
 	$AreaPuny.set_deferred("monitoring", true)
-	$FormaDret.set_deferred("monitoring", false)
-	$Animacio.play("atac")
+	$AreaPuny.set_deferred("monitoring", false)
 
 
 func _on_area_puny_body_entered(body):
