2898be5a74f373e2ee2f6d6642172bf9e80fd985
uriolo <165772030+uriolo@users.noreply.github.com>
2024-05-13 19:48:22 +0200

    flors i monedes arreglades

--- Area2D.gd
+++ Area2D.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 
 @export var intensitat_salt := 700
-@export var velocitatx := 10000
+@export var velocitatx := 100
 func _on_body_entered(body):
 	body.salta(intensitat_salt, velocitatx)
